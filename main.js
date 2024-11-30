0f537ed2c8342ebf65bbbe366df2c31e382adf7f
kate <kate09030506@gmail.com>
2024-11-30 16:04:37 -0400

    git commit

---
+++ main.js
@@ -0,0 +1,67 @@
+//variables
+
+let Myage = 15,
+    Myname = "josefino",
+    Mymessage = "hello :)";
+
+//alert(Myname)
+//alert(Myage)
+//alert(Mymessage)
+
+//constantes
+
+const MybirthDay = "apr-21-2015"
+
+//alert(MybirthDay);
+
+//tipos de datos
+
+let MyAge = 33;
+let IsActive = true;
+let IsBlue = false;
+
+let MyHeight = null;
+let Myheight2 = undefined;
+
+//ingreso de argumentos 
+
+//let year = prompt('año actual:');
+
+//alert(year);
+
+//confirm
+
+let isBoss = confirm('eres el jefe?');
+
+alert(isBoss);
+
+//operadores matematicos
+let num1 = 10;
+let num2 = 20;
+
+// //suma
+// alert(num1 + num2);
+
+// //resta
+// alert(num1 - num2);
+
+// //multiplicacion 
+// alert(num1 * num2);
+
+// //division 
+// alert(num1 / num2);
+
+//incremento
+// let num3 = 2;
+
+// num3++;
+// alert(num3);
+
+//operadores de comparacion
+alert( 2 > 1 );
+alert( 2 < 1);
+alert( 2 == 1 );
+alert( 2 != 1 );
+
+//condicionasles
+
